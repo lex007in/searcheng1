54dc32acc361ec8f0590fea3976a02495a6f4b22
Ivan Nikolaev <ivannik@yandex-team.ru>
2014-10-20 22:47:36 +0400

    add distance queries support

--- indexer.py
+++ indexer.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import sys
 import os
 import re
@@ -9,24 +10,23 @@ __author__ = 'ivannik'
 morph = pymorphy2.MorphAnalyzer()
 
 
+def doc():
+    return defaultdict(list)
+
+
 def index_dir(dir_name):
-    search_index = {}
+    search_index = defaultdict(doc)
     filename_list = []
     global_file_index = 0
+    token_pattern = re.compile(r'[A-Za-zА-Яа-яё\\d-]+')
     for root_dir, dirs, files in os.walk(dir_name):
         for file in files:
             file_path = os.path.normpath(os.path.join(root_dir, file))
             filename_list.append(file_path)
             with open(file_path, encoding='UTF-8') as f:
-                processed = set()
-                for l, line in enumerate(f):
-                    for token in re.finditer(re.compile(r'[A-Za-zА-Яа-яё\\d-]+'), line):
-                        if token.group(0) not in processed:
-                            processed.add(token.group(0))
-                            for term in set([t.normal_form for t in morph.parse(token.group(0))]):
-                                if term not in search_index:
-                                    search_index[term] = set()
-                                search_index[term].add(global_file_index)
+                for pos, token in enumerate(re.finditer(token_pattern, f.read())):
+                    for term in set([t.normal_form for t in morph.parse(token.group(0))]):
+                        search_index[term][global_file_index].append(pos)
                 print('file ' + file_path + ' processed')
                 global_file_index += 1
     return search_index, filename_list
--- searcher.py
+++ searcher.py
@@ -1,30 +1,78 @@
+from collections import defaultdict
 import pickle
 import sys
 import re
+import enum
 
 __author__ = 'ivannik'
 
 
+class QueryType(enum.Enum):
+    q_or = 1
+    q_and = 2
+    q_distance = 3
+
+
+def doc():
+    return defaultdict(list)
+
+
 def validate_prompt(user_input):
     if re.fullmatch(r'[A-Za-zА-Яа-яё\d-]+(\sAND\s[A-Za-zА-Яа-яё\d-]+)*', user_input):
-        return False, user_input.split(' AND ')
+        return QueryType.q_and, user_input.split(' AND ')
     elif re.fullmatch(r'[A-Za-zА-Яа-яё\d-]+(\sOR\s[A-Za-zА-Яа-яё\d-]+)*', user_input):
-        return True, user_input.split(' OR ')
+        return QueryType.q_or, user_input.split(' OR ')
+    elif re.fullmatch(r'[A-Za-zА-Яа-яё\d-]+(\s/[+-]?\d+\s[A-Za-zА-Яа-яё\d-]+)*', user_input):
+        return QueryType.q_distance, user_input.split(' ')
     else:
         return None, None
 
 
-def search(index, words, is_or):
-    if is_or:
+def pos_intersect(p_1, p_2, k):
+    documents = defaultdict(list)
+
+    if k.startswith("/+"):
+        dist = lambda x: x
+    elif k.startswith("/-"):
+        dist = lambda x: x
+    else:
+        dist = abs
+
+    k_int = int(k[1:])
+    common_docs = set(p_1.keys()).intersection(set(p_2.keys()))
+    for doc in common_docs:
+        i1 = 0
+        i2 = 0
+        while i1 < len(p_1[doc]) and i2 < len(p_2[doc]):
+            if dist(p_1[doc][i1] - p_2[doc][i2]) <= k_int:
+                documents[doc].append(p_1[doc][i1])
+                i1 += 1
+            elif p_1[doc][i1] < p_2[doc][i2]:
+                i1 += 1
+            else:
+                i2 += 1
+    return documents
+
+
+def search(index, words, query_type):
+    if query_type == QueryType.q_or:
         documents = set()
         for word in words:
-            documents = documents.union(index.get(word, set()))
+            documents = documents.union(index[word].keys())
         return list(documents)
-    else:
+    elif query_type == QueryType.q_and:
         documents = set(index.get(words[0], set()))
         for word in words:
-            documents = documents.intersection(index.get(word, set()))
+            documents = documents.intersection(index[word].keys())
         return list(documents)
+    elif query_type == QueryType.q_distance:
+        documents = index.get(words[0], defaultdict(list))
+        if len(words) < 3:
+            # never happen
+            return list(documents.keys())
+        for i in range(2, len(words), 2):
+            documents = pos_intersect(documents, index.get(words[i], defaultdict(list)), words[i - 1])
+        return list(documents.keys())
 
 
 def print_results(file_names, file_indexes):
@@ -45,9 +93,9 @@ with open(sys.argv[1], 'rb') as index_file:
         userInput = input('search: ')
         if userInput == 'q':
             break
-        (is_or, query_words) = validate_prompt(userInput)
-        if query_words is None:
+        (q_type, query_words) = validate_prompt(userInput)
+        if q_type is None:
             print('incorrect query')
             continue
-        doc_indexes = search(search_index, query_words, is_or)
+        doc_indexes = search(search_index, query_words, q_type)
         print_results(files_list, doc_indexes)
